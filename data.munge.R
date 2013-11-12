7549519e93074df5c97fcc25ca4b9cce57f57c9b
Carl A. B. Pearson <cap10@ufl.edu>
2013-11-11 21:38:41 -0500

    hist of end times
    superposed with cum. creation and annihilation of loc ids

--- data.munge.R
+++ data.munge.R
@@ -1,5 +1,6 @@
 setwd("~/Dropbox/") #setwd("/Volumes/Data/dropbox/") 
 origin <- as.POSIXct(strptime('2004-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'))
+debug <- F
 
 con <- gzfile("montreal.tgz")
 montreal.df<-read.csv(con,header=F,skip=1, sep=",", col.names=c("user.id","loc.id","start.s","end.s"))
@@ -8,7 +9,7 @@ montreal.df$duration <- montreal.df$end.s - montreal.df$start.s
 ## given the small number of these,
 ## and the unclear origin,
 ## resolve by manual inspection where duration < 0
-dump<-apply(subset(montreal.df,duration<0),1,function(row) {
+if (debug) dump<-apply(subset(montreal.df,duration<0),1,function(row) {
   tot<-subset(montreal.df, user.id == row["user.id"] & loc.id == row["loc.id"] & start.s != row["start.s"] & end.s != row["end.s"] )
   if(dim(tot)[1] == 0) {
     print(row)
@@ -28,22 +29,32 @@ dump<-apply(subset(montreal.df,duration<0),1,function(row) {
     }
   }
 })
+
 ## for our data, these all indicate "SWAP":
 montreal.df[which(montreal.df$duration < 0),c("start.s","end.s")] <- montreal.df[which(montreal.df$duration < 0),c("end.s","start.s")]
 montreal.df[which(montreal.df$duration < 0),"duration"] <- -montreal.df[which(montreal.df$duration < 0),"duration"]
 ## delete alternative: montreal.df <- subset(montreal.df, duration > 0)
 ## another alternative: insert a mean-duration event centered between the start and end?
 
+hour <- 60*60
+window <- 0.5 # half hour
+hist.data <- hist(subset(montreal.df,!is.na(end.s))$end.s/hour, breaks=seq(min(montreal.df$end.s,na.rm=T), max(montreal.df$end.s,na.rm=T)+window*hour, by=window*hour)/hour, plot=F )
+
+plot(hist.data$mids, hist.data$counts/max(hist.data$counts),type="h")
+
 loc.creation <- aggregate(montreal.df$start.s,
     by=list(loc.id = montreal.df$loc.id), min, na.rm=T)
 loc.creation <- loc.creation[with(loc.creation,order(x,loc.id)),]
 loc.creation$acc <- cumsum(rep.int(1,dim(loc.creation)[1]))
-create.x <- loc.creation$x - loc.creation$x[1]
-plot(create.x, loc.creation$acc, type="l", col="green")
+lines(loc.creation$x/hour, loc.creation$acc/max(loc.creation$acc), col="green")
 
 loc.destruction <- aggregate(montreal.df$end.s,
     by=list(loc.id = montreal.df$loc.id), max, na.rm=T)
 loc.destruction <- loc.destruction[with(loc.destruction,order(x,loc.id)),]
+loc.destruction$acc <- cumsum(rep.int(1,dim(loc.destruction)[1]))
+lines(loc.destruction$x/hour, loc.destruction$acc/max(loc.destruction$acc), col="red")
+
+## merge creation and destruction, interpolate for missing times
 
 loc.cutoff <- subset(aggregate(rep.int(1,dim(montreal.df)[1]), by=list(loc.id = montreal.df$loc.id, end.s = montreal.df$end.s), sum), x > 1)
 loc.cutoff <- loc.cutoff[with(loc.cutoff,order(x,loc.id)),]
